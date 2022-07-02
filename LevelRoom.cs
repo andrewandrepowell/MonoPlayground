e59d0e7d6ccd42f6e8ac26f1a5603f9eb1cb378e
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2022-07-01 23:37:00 -0500

    Doing some clean up.

--- LevelRoom.cs
+++ LevelRoom.cs
@@ -33,147 +33,147 @@ namespace MonoPlayground
                 void AddWall<T>(float x, float y) where T : Wall
                 {
                     T wall = (T)Activator.CreateInstance(typeof(T), game.Content);
-                    wall.Physics.Position = new Vector2(x: x, y: y);
+                    wall.Physics.Position = new Vector2(x: Wall.Width * x, y: _roomBounds.Height - Wall.Width * y);
                     walls.Add(wall);
                 };
                 
                 for (int i = 5; i < 12; i++)
-                    AddWall<Wall15>(Wall.Width * 0, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall14>(Wall.Width * 0, _roomBounds.Height - Wall.Width * 4);
-                AddWall<Wall13>(Wall.Width * 1, _roomBounds.Height - Wall.Width * 4);
+                    AddWall<Wall15>(0, i);
+                AddWall<Wall14>(0, 4);
+                AddWall<Wall13>(1, 4);
                 for (int i = 2; i < 5; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 4);
-                AddWall<Wall7>(Wall.Width * 5, _roomBounds.Height - Wall.Width * 4);
-                AddWall<Wall8>(Wall.Width * 5, _roomBounds.Height - Wall.Width * 3);
+                    AddWall<Wall1>(i, 4);
+                AddWall<Wall7>(5, 4);
+                AddWall<Wall8>(5, 3);
                 for (int i = 7; i < 14; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 2);
-                AddWall<Wall9>(Wall.Width * 14, _roomBounds.Height - Wall.Width * 3);
+                    AddWall<Wall1>(i, 2);
+                AddWall<Wall9>(14, 3);
                 for (int i = 4; i < 8; i++)
-                    AddWall<Wall5>(Wall.Width * 15, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall19>(Wall.Width * 15, _roomBounds.Height - Wall.Width * 8);
-                AddWall<Wall18>(Wall.Width * 11, _roomBounds.Height - Wall.Width * 6);
-                AddWall<Wall17>(Wall.Width * 10, _roomBounds.Height - Wall.Width * 6);
-                AddWall<Wall16>(Wall.Width * 9, _roomBounds.Height - Wall.Width * 6);
-                AddWall<Wall20>(Wall.Width * 15, _roomBounds.Height - Wall.Width * 9);
+                    AddWall<Wall5>(15, i);
+                AddWall<Wall19>(15, 8);
+                AddWall<Wall18>(11, 6);
+                AddWall<Wall17>(10, 6);
+                AddWall<Wall16>(9, 6);
+                AddWall<Wall20>(15, 9);
                 for (int i = 8; i < 15; i++)
-                    AddWall<Wall21>(Wall.Width * i, _roomBounds.Height - Wall.Width * 9);
-                AddWall<Wall18>(Wall.Width * 6, _roomBounds.Height - Wall.Width * 6);
-                AddWall<Wall16>(Wall.Width * 5, _roomBounds.Height - Wall.Width * 6);
-                AddWall<Wall23>(Wall.Width * 6, _roomBounds.Height - Wall.Width * 10);
-                AddWall<Wall18>(Wall.Width * 3, _roomBounds.Height - Wall.Width * 7);
-                AddWall<Wall16>(Wall.Width * 2, _roomBounds.Height - Wall.Width * 7);
+                    AddWall<Wall21>(i, 9);
+                AddWall<Wall18>(6, 6);
+                AddWall<Wall16>(5, 6);
+                AddWall<Wall23>(6, 10);
+                AddWall<Wall18>(3, 7);
+                AddWall<Wall16>(2, 7);
                 for (int i = 11; i < 14; i++)
-                    AddWall<Wall5>(Wall.Width * 7, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall19>(Wall.Width * 7, _roomBounds.Height - Wall.Width * 14);
-                AddWall<Wall20>(Wall.Width * 7, _roomBounds.Height - Wall.Width * 15);
+                    AddWall<Wall5>(7, i);
+                AddWall<Wall19>(7, 14);
+                AddWall<Wall20>(7, 15);
                 for (int i = 5; i < 7; i++)
-                    AddWall<Wall21>(Wall.Width * i, _roomBounds.Height - Wall.Width * 15);
-                AddWall<Wall27>(Wall.Width * 4, _roomBounds.Height - Wall.Width * 15);
+                    AddWall<Wall21>(i, 15);
+                AddWall<Wall27>(4, 15);
                 for (int i = 16; i < 17; i++)
-                    AddWall<Wall28>(Wall.Width * 4, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall12>(Wall.Width * 4, _roomBounds.Height - Wall.Width * 17);
-                AddWall<Wall24>(Wall.Width * 2, _roomBounds.Height - Wall.Width * 13);
-                AddWall<Wall21>(Wall.Width * 1, _roomBounds.Height - Wall.Width * 12);
-                AddWall<Wall36>(Wall.Width * 0, _roomBounds.Height - Wall.Width * 12);
+                    AddWall<Wall28>(4, i);
+                AddWall<Wall12>(4, 17);
+                AddWall<Wall24>(2, 13);
+                AddWall<Wall21>(1, 12);
+                AddWall<Wall36>(0, 12);
                 for (int i = 14; i < 18; i++)
-                    AddWall<Wall26>(Wall.Width * 2, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall29>(Wall.Width * 2, _roomBounds.Height - Wall.Width * 18);
-                AddWall<Wall25>(Wall.Width * 2, _roomBounds.Height - Wall.Width * 19);
+                    AddWall<Wall26>(2, i);
+                AddWall<Wall29>(2, 18);
+                AddWall<Wall25>(2, 19);
                 for (int i = 3; i < 8; i++)
-                    AddWall<Wall21>(Wall.Width * i, _roomBounds.Height - Wall.Width * 19);
-                AddWall<Wall11>(Wall.Width * 8, _roomBounds.Height - Wall.Width * 19);
-                AddWall<Wall10>(Wall.Width * 8, _roomBounds.Height - Wall.Width * 20);
-                AddWall<Wall30>(Wall.Width * 6, _roomBounds.Height - Wall.Width * 21);
+                    AddWall<Wall21>(i, 19);
+                AddWall<Wall11>(8, 19);
+                AddWall<Wall10>(8, 20);
+                AddWall<Wall30>(6, 21);
                 for (int i = 4; i < 6; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 21);
-                AddWall<Wall8>(Wall.Width * 2, _roomBounds.Height - Wall.Width * 22);
+                    AddWall<Wall1>(i, 21);
+                AddWall<Wall8>(2, 22);
                 for (int i = 23; i < 27; i++)
-                    AddWall<Wall26>(Wall.Width * 2, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall33>(Wall.Width * 2, _roomBounds.Height - Wall.Width * 28);
+                    AddWall<Wall26>(2, i);
+                AddWall<Wall33>(2, 28);
                 for (int i = 4; i < 7; i++)
-                    AddWall<Wall34>(Wall.Width * i, _roomBounds.Height - Wall.Width * 28);
-                AddWall<Wall35>(Wall.Width * 7, _roomBounds.Height - Wall.Width * 28);
+                    AddWall<Wall34>(i, 28);
+                AddWall<Wall35>(7, 28);
                 for (int i = 29; i * Wall.Width <= _roomBounds.Height; i++)
-                    AddWall<Wall15>(Wall.Width * 7, _roomBounds.Height - Wall.Width * i);
+                    AddWall<Wall15>(7, i);
                 for (int i = 5; i < 8; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 17);
-                AddWall<Wall30>(Wall.Width * 8, _roomBounds.Height - Wall.Width * 17);
-                AddWall<Wall30>(Wall.Width * 10, _roomBounds.Height - Wall.Width * 16);
-                AddWall<Wall7>(Wall.Width * 12, _roomBounds.Height - Wall.Width * 15);
+                    AddWall<Wall1>(i, 17);
+                AddWall<Wall30>(8, 17);
+                AddWall<Wall30>(10, 16);
+                AddWall<Wall7>(12, 15);
                 for (int i = 14; i < 15; i++)
-                    AddWall<Wall26>(Wall.Width * 12, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall8>(Wall.Width * 12, _roomBounds.Height - Wall.Width * 13);
+                    AddWall<Wall26>(12, i);
+                AddWall<Wall8>(12, 13);
                 for (int i = 14; i < 20; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 12);
-                AddWall<Wall31>(Wall.Width * 20, _roomBounds.Height - Wall.Width * 12);
-                AddWall<Wall32>(Wall.Width * 21, _roomBounds.Height - Wall.Width * 12);
+                    AddWall<Wall1>(i, 12);
+                AddWall<Wall31>(20, 12);
+                AddWall<Wall32>(21, 12);
                 for (int i = 13; i < 28; i++)
-                    AddWall<Wall28>(Wall.Width * 21, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall12>(Wall.Width * 21, _roomBounds.Height - Wall.Width * 28);
-                AddWall<Wall18>(Wall.Width * 14, _roomBounds.Height - Wall.Width * 21);
-                AddWall<Wall16>(Wall.Width * 13, _roomBounds.Height - Wall.Width * 21);
-                AddWall<Wall23>(Wall.Width * 15, _roomBounds.Height - Wall.Width * 28);
-                AddWall<Wall4>(Wall.Width * 16, _roomBounds.Height - Wall.Width * 29);
-                AddWall<Wall10>(Wall.Width * 17, _roomBounds.Height - Wall.Width * 29);
-                AddWall<Wall15>(Wall.Width * 17, _roomBounds.Height - Wall.Width * 28);
-                AddWall<Wall11>(Wall.Width * 17, _roomBounds.Height - Wall.Width * 27);
+                    AddWall<Wall28>(21, i);
+                AddWall<Wall12>(21, 28);
+                AddWall<Wall18>(14, 21);
+                AddWall<Wall16>(13, 21);
+                AddWall<Wall23>(15, 28);
+                AddWall<Wall4>(16, 29);
+                AddWall<Wall10>(17, 29);
+                AddWall<Wall15>(17, 28);
+                AddWall<Wall11>(17, 27);
                 for (int i = 22; i < 24; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 28);
-                AddWall<Wall10>(Wall.Width * 24, _roomBounds.Height - Wall.Width * 28);
+                    AddWall<Wall1>(i, 28);
+                AddWall<Wall10>(24, 28);
                 for (int i = 15; i < 28; i++)
-                    AddWall<Wall15>(Wall.Width * 24, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall14>(Wall.Width * 24, _roomBounds.Height - Wall.Width * 14);
-                AddWall<Wall13>(Wall.Width * 25, _roomBounds.Height - Wall.Width * 14);
+                    AddWall<Wall15>(24, i);
+                AddWall<Wall14>(24, 14);
+                AddWall<Wall13>(25, 14);
                 for (int i = 25; i < 36; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 14);
-                AddWall<Wall32>(Wall.Width * 36, _roomBounds.Height - Wall.Width * 14);
+                    AddWall<Wall1>(i, 14);
+                AddWall<Wall32>(36, 14);
                 for (int i = 15; i < 25; i++)
-                    AddWall<Wall28>(Wall.Width * 36, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall18>(Wall.Width * 31, _roomBounds.Height - Wall.Width * 28);
-                AddWall<Wall16>(Wall.Width * 30, _roomBounds.Height - Wall.Width * 28);
-                AddWall<Wall12>(Wall.Width * 36, _roomBounds.Height - Wall.Width * 25);
-                AddWall<Wall10>(Wall.Width * 37, _roomBounds.Height - Wall.Width * 25);
+                    AddWall<Wall28>(36, i);
+                AddWall<Wall18>(31, 28);
+                AddWall<Wall16>(30, 28);
+                AddWall<Wall12>(36, 25);
+                AddWall<Wall10>(37, 25);
                 for (int i = 14; i < 25; i++)
-                    AddWall<Wall15>(Wall.Width * 37, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall11>(Wall.Width * 37, _roomBounds.Height - Wall.Width * 13);
+                    AddWall<Wall15>(37, i);
+                AddWall<Wall11>(37, 13);
                 for (int i = 4; Wall.Width * i <= _roomBounds.Height; i++)
-                    AddWall<Wall5>(Wall.Width * 39, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall9>(Wall.Width * 38, _roomBounds.Height - Wall.Width * 3);
+                    AddWall<Wall5>(39, i);
+                AddWall<Wall9>(38, 3);
                 for (int i = 18; i < 38; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 2);
-                AddWall<Wall8>(Wall.Width * 16, _roomBounds.Height - Wall.Width * 3);
+                    AddWall<Wall1>(i, 2);
+                AddWall<Wall8>(16, 3);
                 for (int i = 4; i < 7; i++)
-                    AddWall<Wall26>(Wall.Width * 16, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall29>(Wall.Width * 16, _roomBounds.Height - Wall.Width * 7);
-                AddWall<Wall25>(Wall.Width * 16, _roomBounds.Height - Wall.Width * 8);
-                AddWall<Wall11>(Wall.Width * 17, _roomBounds.Height - Wall.Width * 8);
+                    AddWall<Wall26>(16, i);
+                AddWall<Wall29>(16, 7);
+                AddWall<Wall25>(16, 8);
+                AddWall<Wall11>(17, 8);
                 for (int i = 9; i < 11; i++)
-                    AddWall<Wall15>(Wall.Width * 17, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall36>(Wall.Width * 17, _roomBounds.Height - Wall.Width * 11);
+                    AddWall<Wall15>(17, i);
+                AddWall<Wall36>(17, 11);
                 for (int i = 18; i < 22; i++)
-                    AddWall<Wall21>(Wall.Width * i, _roomBounds.Height - Wall.Width * 11);
-                AddWall<Wall11>(Wall.Width * 22, _roomBounds.Height - Wall.Width * 11);
-                AddWall<Wall15>(Wall.Width * 22, _roomBounds.Height - Wall.Width * 12);
-                AddWall<Wall36>(Wall.Width * 22, _roomBounds.Height - Wall.Width * 13);
+                    AddWall<Wall21>(i, 11);
+                AddWall<Wall11>(22, 11);
+                AddWall<Wall15>(22, 12);
+                AddWall<Wall36>(22, 13);
                 for (int i = 23; i < 37; i++)
-                    AddWall<Wall21>(Wall.Width * i, _roomBounds.Height - Wall.Width * 13);
-                AddWall<Wall23>(Wall.Width * 19, _roomBounds.Height - Wall.Width * 6);
-                AddWall<Wall21>(Wall.Width * 21, _roomBounds.Height - Wall.Width * 5);
-                AddWall<Wall21>(Wall.Width * 22, _roomBounds.Height - Wall.Width * 5);
-                AddWall<Wall11>(Wall.Width * 23, _roomBounds.Height - Wall.Width * 5);
+                    AddWall<Wall21>(i, 13);
+                AddWall<Wall23>(19, 6);
+                AddWall<Wall21>(21, 5);
+                AddWall<Wall21>(22, 5);
+                AddWall<Wall11>(23, 5);
                 for (int i = 6; i < 9; i++)
-                    AddWall<Wall15>(Wall.Width * 23, _roomBounds.Height - Wall.Width * i);
-                AddWall<Wall5>(Wall.Width * 20, _roomBounds.Height - Wall.Width * 7);
-                AddWall<Wall5>(Wall.Width * 20, _roomBounds.Height - Wall.Width * 8);
-                AddWall<Wall4>(Wall.Width * 20, _roomBounds.Height - Wall.Width * 9);
+                    AddWall<Wall15>(23, i);
+                AddWall<Wall5>(20, 7);
+                AddWall<Wall5>(20, 8);
+                AddWall<Wall4>(20, 9);
                 for (int i = 21; i < 23; i++)
-                    AddWall<Wall1>(Wall.Width * i, _roomBounds.Height - Wall.Width * 9);
-                AddWall<Wall10>(Wall.Width * 23, _roomBounds.Height - Wall.Width * 9);
-                AddWall<Wall23>(Wall.Width * 33, _roomBounds.Height - Wall.Width * 10);
-                AddWall<Wall10>(Wall.Width * 35, _roomBounds.Height - Wall.Width * 11);
-                AddWall<Wall15>(Wall.Width * 35, _roomBounds.Height - Wall.Width * 10);
-                AddWall<Wall11>(Wall.Width * 35, _roomBounds.Height - Wall.Width * 9);
-                AddWall<Wall4>(Wall.Width * 34, _roomBounds.Height - Wall.Width * 11);
+                    AddWall<Wall1>(i, 9);
+                AddWall<Wall10>(23, 9);
+                AddWall<Wall23>(33, 10);
+                AddWall<Wall10>(35, 11);
+                AddWall<Wall15>(35, 10);
+                AddWall<Wall11>(35, 9);
+                AddWall<Wall4>(34, 11);
             }
 
             List<DisplayFeature> textures = new List<DisplayFeature>();
@@ -183,42 +183,42 @@ namespace MonoPlayground
                     DisplayFeature display = new DisplayFeature(
                         gameObject: this, 
                         texture: game.Content.Load<Texture2D>(texture));
-                    display.Position = new Vector2(x, y);
+                    display.Position = new Vector2(Wall.Width * x, _roomBounds.Height - Wall.Width * y);
                     textures.Add(display);
                 };
                 
                 for (int i = 0; Wall.Width * i < _roomBounds.Width; i++)
-                    AddTexture("object3Texture", Wall.Width * i, _roomBounds.Height - Wall.Width * 1);
+                    AddTexture("object3Texture",i, 1);
                 for (int x = 0; x < 5; x++)
                     for (int y = 0; y < 4; y++)
-                        AddTexture("object3Texture", Wall.Width * x, _roomBounds.Height - Wall.Width * y);
+                        AddTexture("object3Texture", x, y);
                 for (int x = 0; x < 2; x++)
                     for (int y = 12; Wall.Width * y <= _roomBounds.Height; y++)
-                        AddTexture("object3Texture", Wall.Width * x, _roomBounds.Height - Wall.Width * y);
+                        AddTexture("object3Texture", x, y);
                 for (int i = 2; i < 6; i++)
-                    AddTexture("object3Texture", Wall.Width * i, _roomBounds.Height - Wall.Width * 20);
+                    AddTexture("object3Texture", i, 20);
                 for (int x = 2; x < 7; x++)
                     for (int y = 28; Wall.Width * y <= _roomBounds.Height; y++)
-                        AddTexture("object3Texture", Wall.Width * x, _roomBounds.Height - Wall.Width * y);
+                        AddTexture("object3Texture", x, y);
                 for (int i = 5; i < 8; i++)
-                    AddTexture("object3Texture", Wall.Width * i, _roomBounds.Height - Wall.Width * 16);
-                AddTexture("object3Texture", Wall.Width * 8, _roomBounds.Height - Wall.Width * 15);
-                AddTexture("object3Texture", Wall.Width * 9, _roomBounds.Height - Wall.Width * 15);
+                    AddTexture("object3Texture", i, 16);
+                AddTexture("object3Texture", 8, 15);
+                AddTexture("object3Texture", 9, 15);
                 for (int i = 8; i < 12; i++)
                     AddTexture("object3Texture", Wall.Width * i, _roomBounds.Height - Wall.Width * 14);
                 for (int y = 10; y < 14; y++)
                     for (int x = 8; x < 12; x++)
-                        AddTexture("object3Texture", Wall.Width * x, _roomBounds.Height - Wall.Width * y);
-                AddTexture("object3Texture", Wall.Width * 16, _roomBounds.Height - Wall.Width * 9);
+                        AddTexture("object3Texture", x, y);
+                AddTexture("object3Texture", 16, 9);
                 for (int y = 10; y < 12; y++)
                     for (int x = 12; x < 17; x++)
-                        AddTexture("object3Texture", Wall.Width * x, _roomBounds.Height - Wall.Width * y);
+                        AddTexture("object3Texture", x, y);
                 for (int y = 14; y < 28; y++)
                     for (int x = 22; x < 24; x++)
-                        AddTexture("object3Texture", Wall.Width * x, _roomBounds.Height - Wall.Width * y);
+                        AddTexture("object3Texture", x, y);
                 for (int y = 6; y < 9; y++)
                     for (int x = 21; x < 23; x++)
-                        AddTexture("object3Texture", Wall.Width * x, _roomBounds.Height - Wall.Width * y);
+                        AddTexture("object3Texture", x, y);
             }
 
             List<Bouncer> bouncers = new List<Bouncer>();
